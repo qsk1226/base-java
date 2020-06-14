329b1fe217b986c336471d982cbc90aa4dd9ef31
qinshengke <qinshengkei@taoche.com>
2020-06-14 10:42:21 +0800

    观察者模式示例

--- src/main/java/com/goddess/base/design_model/observer/Client.java
+++ src/main/java/com/goddess/base/design_model/observer/Client.java
@@ -1,17 +1,21 @@
 package com.goddess.base.design_model.observer;
-
+/**
+ * 观察者模式客户端
+ *
+ * @author qinshengke
+ * @since 2020/6/14 10:41
+ **/
 public class Client {
-    public static void main(String[] args) {
-        SubscriptionSubject mSubscriptionSubject=new SubscriptionSubject();
-        //创建微信用户
-        GuardianObserver user1=new GuardianObserver("杨影枫");
-        GuardianObserver user2=new GuardianObserver("月眉儿");
-        GuardianObserver user3=new GuardianObserver("紫轩");
-        //订阅公众号
-        mSubscriptionSubject.addObserver(user1);
-        mSubscriptionSubject.addObserver(user2);
-        mSubscriptionSubject.addObserver(user3);
-        //公众号更新发出消息给订阅的微信用户
-        mSubscriptionSubject.notifyObserver("刘望舒的专栏更新了");
-    }
+
+	public static void main(String[] args) {
+		SubscriptionSubject mSubscriptionSubject = new SubscriptionSubject();
+		//创建监护人
+		GuardianObserver user1 = new GuardianObserver("孩儿他爹");
+		GuardianObserver user2 = new GuardianObserver("孩儿他娘");
+		// 添加监护
+		mSubscriptionSubject.addObserver(user1);
+		mSubscriptionSubject.addObserver(user2);
+		// 通知打架的通知
+		mSubscriptionSubject.notifyObserver("孩儿又在学校打架了");
+	}
 }

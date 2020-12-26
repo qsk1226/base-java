c9bbb3269b95b520674df8955c226ff86b53d688
qsk1226 <15723023+qsk1226@users.noreply.github.com>
2020-12-26 12:33:09 +0800

    锁膨胀

---
+++ src/main/java/com/goddess/base/concurrent/lock/BooleanLatch.java
@@ -0,0 +1,63 @@
+package com.goddess.base.concurrent.lock;
+
+import com.goddess.base.concurrent.util.SleepTools;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class BooleanLatch {
+
+	private static class Sync extends AbstractQueuedSynchronizer {
+		boolean isSignalled() {
+			return getState() != 0;
+		}
+
+		protected int tryAcquireShared(int ignore) {
+			return isSignalled() ? 1 : -1;
+		}
+
+		protected boolean tryReleaseShared(int ignore) {
+			setState(ignore);
+			return true;
+		}
+	}
+
+	private final Sync sync = new Sync();
+
+	public boolean isSignalled() {
+		return sync.isSignalled();
+	}
+
+	public void signal() {
+		sync.releaseShared(1);
+	}
+
+	public void await() throws InterruptedException {
+		sync.acquireSharedInterruptibly(1);
+	}
+
+	public static void main(String[] args) {
+		BooleanLatch booleanLatch = new BooleanLatch();
+
+		new Thread(() -> {
+			try {
+				booleanLatch.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + "我进门了");
+
+		}).start();
+
+		new Thread(() -> {
+			try {
+				booleanLatch.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + "我进门了");
+		}).start();
+
+		SleepTools.second(10);
+		booleanLatch.signal();
+	}
+}

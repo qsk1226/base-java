c939e2f5264ad9408b668223e0233c5253b3eaf2
qsk1226 <15723023+qsk1226@users.noreply.github.com>
2021-03-16 15:22:09 +0800

    算法

---
+++ src/main/java/com/goddess/base/algorithm/link/XX.java
@@ -0,0 +1,41 @@
+package com.goddess.base.algorithm.link;
+
+import java.util.Stack;
+
+/**
+ * .
+ *
+ * @author qinshengke
+ * @since 2021/3/15
+ **/
+public class XX {
+
+	public void ls(MyNode head1, MyNode head2) {
+		Stack<Integer> stack1 = new Stack<>();
+		Stack<Integer> stack2 = new Stack<>();
+
+		while (head1 != null) {
+			stack1.push(head1.value);
+			head1 = head1.next;
+		}
+		while (head2 != null) {
+			stack2.push(head2.value);
+			head2 = head2.next;
+		}
+
+		MyNode result = null;
+		int ca = 0;
+
+		while (!stack1.empty() || !stack2.empty()) {
+			int n1 = stack1.empty() ? 0 : stack1.pop();
+			int n2 = stack2.empty() ? 0 : stack2.pop();
+
+			int sum = n1 + n2 + ca;
+			int cu = sum % 10;
+			ca = sum/10;
+
+		}
+
+
+	}
+}

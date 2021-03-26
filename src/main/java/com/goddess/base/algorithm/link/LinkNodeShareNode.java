5156f78d786980f1410933989149353344c9344f
qsk1226 <15723023+qsk1226@users.noreply.github.com>
2021-03-26 15:56:29 +0800

    算法

--- src/main/java/com/goddess/base/algorithm/link/LinkNodeShareNode.java
+++ src/main/java/com/goddess/base/algorithm/link/LinkNodeShareNode.java
@@ -8,18 +8,39 @@ package com.goddess.base.algorithm.link;
  **/
 public class LinkNodeShareNode {
 
-	public MyNode xx(MyNode head1, MyNode head2) {
+	public static MyNode xx(MyNode head1, MyNode head2) {
 
 		MyNode a = head1;
 		MyNode b = head2;
 
 		while (a != b) {
-			a = a.next ==null ? head2: a.next;
-			b = b.next ==null ?head1: b.next;
+			a = a.next == null ? head2 : a.next;
+			b = b.next == null ? head1 : b.next;
 		}
 		return a;
 	}
 
+	public static void main(String[] args) {
+		MyNode n6 = new MyNode(6);
+		n6.next = new MyNode(7);
+		n6.next.next = new MyNode(8);
 
+		MyNode head = new MyNode(1);
+		head.next = new MyNode(2);
+		head.next.next = new MyNode(3);
+		head.next.next.next = new MyNode(4);
+		head.next.next.next.next = new MyNode(5);
+		head.next.next.next.next.next = n6;
+
+		MyNode head2 = new MyNode(11);
+		head2.next = new MyNode(21);
+		head2.next.next = new MyNode(31);
+		head2.next.next.next = new MyNode(41);
+		head2.next.next.next.next = new MyNode(51);
+		head2.next.next.next.next.next = n6;
+
+		System.out.println(xx(head, head2).value);
+
+	}
 
 }
